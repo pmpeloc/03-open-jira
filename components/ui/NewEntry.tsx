1cbf7b4e51670ba3c70815630848f97b96deab49
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-04-02 13:01:43 -0300

    feat: Form new Entry added

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, TextField } from '@mui/material';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
+export const NewEntry = () => {
+  return (
+    <Box sx={{ marginBotton: 2, paddingX: 2 }}>
+      <Button startIcon={<AddIcon />} fullWidth variant='outlined'>
+        Agregar Tarea
+      </Button>
+      <TextField
+        fullWidth
+        sx={{ marginTop: 2, marginBottom: 1 }}
+        placeholder='Nueva entrada'
+        autoFocus
+        multiline
+        label='Nueva entrada'
+        helperText='Ingrese un valor'
+      />
+      <Box display='flex' justifyContent='space-between'>
+        <Button variant='text'>Cancelar</Button>
+        <Button
+          variant='outlined'
+          color='secondary'
+          endIcon={<SaveOutlinedIcon />}>
+          Guardar
+        </Button>
+      </Box>
+    </Box>
+  );
+};
