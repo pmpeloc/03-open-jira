fcc1572591680a41160fe5c23e7b74b0ff5d1b29
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-04-15 11:24:34 -0300

    feat: Post new entry added

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -16,6 +16,8 @@ export default function handler(
   switch (req.method) {
     case 'GET':
       return getEntries(res);
+    case 'POST':
+      return postEntry(req, res);
     default:
       return res.status(404).json({ message: 'Not Found' });
   }
@@ -26,4 +28,24 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   const entries = await Entry.find().sort({ createdAt: 'ascending' });
   await db.disconnect();
   res.status(200).json(entries);
+};
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description } = req.body;
+
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  });
+
+  try {
+    await db.connect();
+    await newEntry.save();
+    await db.disconnect();
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(500).json({ message: 'Error in save new entry' });
+  }
 };
